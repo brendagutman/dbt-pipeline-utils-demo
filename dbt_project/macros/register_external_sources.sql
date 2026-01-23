100c5aaf523d027c7b60504eae37c9e5f1d60110
brendagutman <brenda.gutman@gmail.com>
2026-01-23 15:31:40 -0600

    Remove macros

--- dbt_project/macros/register_external_sources.sql
+++
@@ -1,4 +0,0 @@
-{% macro register_external_sources(fq_tablename, csv_path) %}
-    {% do adapter.execute("CREATE SCHEMA IF NOT EXISTS " ~ fq_tablename ~ ";") %}
-    {% do adapter.execute("CREATE OR REPLACE VIEW " ~ fq_tablename ~ " AS SELECT * FROM read_csv_auto('" ~ csv_path ~ "');") %}
-{% endmacro %}
